c1ecb62dfffd751c034661453c588015472c7852
Anderson Fonseca <cf.anderson@gmail.com>
2021-07-19 17:57:41 +0900

    Add test case for tstring_startswith_v4.c

--- tlib/tests/tstring/tstring_startswith_v4.c
+++ tlib/tests/tstring/tstring_startswith_v4.c
@@ -18,24 +18,29 @@ typedef struct
 {
         char given[BUFFER_SIZE];
         char comparison[BUFFER_SIZE];
+        int offset;
         bool expected;
 } stringtest;
 
 stringtest st[] = {
-        {"abcdefghij", "a",          true},
-        {"abcdefghij", "ab",         true},
-        {"abcdefghij", "abc",        true},
-        {"abcdefghij", "abcd",       true},
-        {"abcdefghij", "abcdefghij", true},
-        {"abcdefghij", "k",          false},
-        {"abcdefghij", "kl",         false},
-        {"abcdefghij", "klm",        false},
-        {"mnopqrstuv", "a",          false},
-        {"mnopqrstuv", "ab",         false},
-        {"mnopqrstuv", "abc",        false}
+        {"abcdefghijk", "a",          0, true},
+        {"abcdefghijk", "ab",         0, true},
+        {"abcdefghijk", "abc",        0, true},
+        {"abcdefghijk", "abcd",       0, true},
+        {"abcdefghijk", "abcdefghij", 0, true},
+        {"abcdefghijk", "k",          0, false},
+        {"abcdefghijk", "kl",         0, false},
+        {"abcdefghijk", "klm",        1, false},
+        {"mnopqrstuvx", "a",          2, false},
+        {"mnopqrstuvx", "ab",         3, false},
+        {"mnopqrstuvx", "abc",        4, false},
+        {"abcdefghijk", "b",          1, true},
+        {"abcdefghijk", "cd",         2, true},
+        {"abcdefghijk", "efg",        4, true},
+        {"abcdefghijk", "hijk",       7, true},
 };
 
-static void __test_string_startswith_v3(void** state)
+static void __test_string_startswith_v4(void** state)
 {
         for (size_t i = 0; i < sizeof(st)/sizeof(st[0]); i++)
         {
@@ -46,7 +51,8 @@ static void __test_string_startswith_v3(void** state)
                 printf("Test string: given: %s, comparison: %s\n", st[i].given,
                        st[i].comparison);
 
-                assert_int_equal(tstring_startswith_v3(s1, s2), st[i].expected);
+                assert_int_equal(tstring_startswith_v4(s1, s2, st[i].offset),
+                                 st[i].expected);
                 tstring_free(s1);
                 tstring_free(s2);
         }        
@@ -55,7 +61,7 @@ static void __test_string_startswith_v3(void** state)
 int main(void)
 {
         const struct CMUnitTest tests[] = {
-                cmocka_unit_test(__test_string_startswith_v3),
+                cmocka_unit_test(__test_string_startswith_v4),
         };
         return cmocka_run_group_tests(tests, NULL, NULL);
 }
