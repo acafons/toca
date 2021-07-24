cb6917ce8b3f3899634534dbca4ca72fb70e4395
Anderson Fonseca <cf.anderson@gmail.com>
2021-07-24 13:47:28 +0900

    Refactor the test for tstring_length

--- tlib/tests/tstring/tstring_length.c
+++ tlib/tests/tstring/tstring_length.c
@@ -9,34 +9,70 @@
 #include <stdarg.h>
 #include <cmocka.h>
 #include <string.h>
+#include <stdio.h>
 
 
-static void __test_string_not_empty(void** state)
+#define BUFFER_SIZE 101
+
+typedef struct
 {
-        char* str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        tstring* s = tstring_new(str);
+        char given[BUFFER_SIZE];
+        int expected;
+} stringtest;
 
-        assert_non_null(s);
-        assert_int_equal(tstring_length(s), strlen(str));
+stringtest st[] = {
+        {"", 0},
+        {"a", 1},
+        {"ab", 2},
+        {"abc", 3},
+        {"abcd", 4},
+        {"abcde", 5},
+        {"abcdef", 6},
+        {"abcdefg", 7},
+        {"abcdefgh", 8},
+        {"abcdefghi", 9},
+        {"abcdefghij", 10},
+        {"abcdefghijk", 11},
+        {"abcdefghijkl", 12},
+        {"abcdefghijklm", 13},
+        {"abcdefghijklmn", 14},
+        {"abcdefghijklmno", 15},
+        {"abcdefghijklmnop", 16},
+        {"abcdefghijklmnopq", 17},
+        {"abcdefghijklmnopqr", 18},
+        {"abcdefghijklmnopqrs", 19},
+        {"abcdefghijklmnopqrst", 20},
+        {"abcdefghijklmnopqrstu", 21},
+        {"abcdefghijklmnopqrstuv", 22},
+        {"abcdefghijklmnopqrstuvw", 23},
+        {"abcdefghijklmnopqrstuvwx", 24},
+        {"abcdefghijklmnopqrstuvwxy", 25},
+        {"abcdefghijklmnopqrstuvwxyz", 26},
+        {"klmnopqrstuvxwyabcdefghijklmnopqrstuvxwyabcdefghij" \
+         "klmnopqrstuvxwyabcdefghijklmnopqrstuvxwyabcdefghij",
+         100},
+};
 
-        tstring_free(s);
-}
-
-static void __test_string_empty(void** state)
+static void __test_string_length(void** state)
 {
-        tstring* s = tstring_new_v4();
+        for (size_t i = 0; i < sizeof(st)/sizeof(st[0]); i++)
+        {
+                tstring* s = tstring_new(st[i].given);
+                assert_non_null(s);
 
-        assert_non_null(s);
-        assert_int_equal(tstring_length(s), 0);
+                printf("Test (%li): given: %s, expected: %i\n", i + 1,
+                       st[i].given, st[i].expected);
 
-        tstring_free(s);
+                assert_int_equal(tstring_length(s), st[i].expected);
+
+                tstring_free(s);
+        }        
 }
 
 int main(void)
 {
         const struct CMUnitTest tests[] = {
-                cmocka_unit_test(__test_string_not_empty),
-                cmocka_unit_test(__test_string_empty),
+                cmocka_unit_test(__test_string_length),
         };
         return cmocka_run_group_tests(tests, NULL, NULL);
 }
